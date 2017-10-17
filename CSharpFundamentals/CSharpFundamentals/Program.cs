4f7a5973ae37eeebe7f35d1357c0024fb84a0b12
Livepurposefully <nafissa_92@hotmail.com>
2017-10-17 15:58:40 -0400

    Finish Arrays demo

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -12,13 +12,31 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            var nafissa= new Person();
+            /*var nafissa= new Person();
             nafissa.FirstName = "Nafissa";
             nafissa.LastName = "Hassan";
             nafissa.Introduce();
             Calculator caclulator= new Calculator();
             var result= caclulator.Add(1, 2);
-            Console.WriteLine(result);
+            Console.WriteLine(result);*/
+
+            var numbers = new int [3];
+            numbers[0] = 1;
+
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var names = new string[3] {"Nafissa", "Hind", "Louis"};
+            Console.WriteLine(names [0]);
+
         }
     }
 }
