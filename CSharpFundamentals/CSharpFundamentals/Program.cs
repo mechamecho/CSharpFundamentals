835bfcbbd75cf17f385e2af8e3e899f2dbcff6db
Livepurposefully <nafissa_92@hotmail.com>
2017-10-17 15:08:22 -0400

    Finish classes demo on CSharp

---
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpFundamentals.Math;
+
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nafissa= new Person();
+            nafissa.FirstName = "Nafissa";
+            nafissa.LastName = "Hassan";
+            nafissa.Introduce();
+            Calculator caclulator= new Calculator();
+            var result= caclulator.Add(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
